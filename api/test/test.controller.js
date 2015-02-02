21bc3d648413a92382ca782b39bc3665368df549
Dan Taylor <dt.raelcun@gmail.com>
2015-02-01 19:51:50 -0500

    Merge dan-refactor into refactor-ARCustomer

--- api/test/test.controller.js
+++ api/test/test.controller.js
@@ -1,37 +1,60 @@
-var path = require('path');
-var appDir = path.dirname(require.main.filename);
-var querystring = require('querystring');
-var request = require('request');
-var ordersCtl = require(appDir + '/api/orders/orders.controller');
-var SDParse=require(appDir + '/SDataLib/SDParse');
-var xmldoc = require('xmldoc');
-var _ = require('lodash');
-var soapWSDL = "http://demo.aspdotnetstorefront.martinandassoc.com/ipx.asmx?wsdl";
-
 var xml2json = require('xml2json');
 var SFLib = require('../../SFLib');
 var Q = require('q');
 var config = require('../../config');
+var ARCustomer = require('../../SDataLib/ARCustomer');
 
-exports.sdata = function(req,res) {
-	ordersCtl.index(req,res,function(err,orders){
-		configObj = req.body.sdata;
-		records = orders.Records;
-		var query = "SO_SalesOrderHeaderSPECIAL?include=SO_SalesOrderHeaderSPECIALSECOND";
-		//var	query = "SO_InvoiceHeaderSPECIAL?include=SO_InvoiceHeaderSPECIALSECOND";
-		configObj.query = query;
-		SDParse.Get(configObj,function(err,data){
-			//console.log(data);
-			//console.log(data.SO_InvoiceHeaderSPECIAL.SO_InvoiceHeaderSPECIALSECOND.LineKey);
-			var tst = JSON.stringify(data);
-			res.type('application/json');
-			res.send(tst);
-		});
-	});
+exports.sdata = function(req, res) {
+	ARCustomer
+		.doCustomersExistQ(req.body.sdata.url, req.body.sdata.auth.username, req.body.sdata.auth.password, req.body.sdata.company,
+			[
+				'corey.durthaler@martinandassoc.com',
+				'shawn.goodwin@martinandassoc.com',
+				'dt.raelcun@gmail.com'
+			]
+		)
+		.then(function (results) {
+			res.send(results);
+		})
+		.done();
 };
 
-exports.getOrders = function(req, res) {
+exports.testCreateCustomers = function (req, res) {
+	ARCustomer
+		.createCustomersQ(req.body.sdata.url, req.body.sdata.auth.username, req.body.sdata.auth.password, req.body.sdata.company,
+			[
+				{
+					emailAddress: 'corey.durthaler@martinandassoc.com',
+				},
+				{
+					emailAddress: 'shawn.goodwin@martinandassoc.com',
+					firstName: 'ShawnRocksOhio'
+				},
+				{
+					emailAddress: 'dt.raelcun@gmail.com',
+					firstName: 'Dan',
+					lastName: 'Taylor'
+				}
+			])
+		.then(function (results) {
+			res.send(results);
+		})
+		.done();
+};
 
+exports.testCustomersExist = function(req, res) {
+	ARCustomer
+		.doCustomersExistQ(req.body.sdata.url, req.body.sdata.auth.username, req.body.sdata.auth.password, req.body.sdata.company,
+			[
+				'corey.durthaler@martinandassoc.com',
+				'shawn.goodwin@martinandassoc.com',
+				'dt.raelcun@gmail.com'
+			]
+		)
+		.then(function (results) {
+			res.send(results);
+		})
+		.done();
 };
 
 exports.sflib = function(req, res) { // refactored
